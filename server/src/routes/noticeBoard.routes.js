e0afb79438add8943b3e6d16d09b474fbb500227
samir suroshe <sameersuroshe50@gmail.com>
2025-07-31 12:40:41 +0530

    fix: update notice board routes to use verifySectorAdmin middleware

--- server/src/routes/noticeBoard.routes.js
+++ server/src/routes/noticeBoard.routes.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { createNotice, deleteNotice, getNotices, updateNotice } from "../controllers/noticeBoard.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { verifyAdmin } from "../middlewares/admin.middleware.js";
+import { verifySectorAdmin } from "../middlewares/sectoradmin.middleware.js";
 
 const router = Router();
 
-router.route('/create-notice').post(verifyJwt, verifyAdmin, upload.single("file"), createNotice);
+router.route('/create-notice').post(verifyJwt, verifySectorAdmin, upload.single("file"), createNotice);
 router.route('/get-notices').get(getNotices);
-router.route('/update-notice/:id').put(verifyJwt, verifyAdmin, upload.single("file"), updateNotice);
-router.route('/delete-notice/:id').delete(verifyJwt, verifyAdmin, deleteNotice)
+router.route('/update-notice/:id').put(verifyJwt, verifySectorAdmin, upload.single("file"), updateNotice);
+router.route('/delete-notice/:id').delete(verifyJwt, verifySectorAdmin, deleteNotice)
 
 export default router;
